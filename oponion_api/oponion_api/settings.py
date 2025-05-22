530b8dec6a922792f3d0174ee64fab84f640aad6
TheRealSpeedie <119527242+TheRealSpeedie@users.noreply.github.com>
2025-05-22 11:08:10 +0200

    Update settings.py

--- oponion_api/oponion_api/settings.py
+++ oponion_api/oponion_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@0mpa!=jdbc23+s!ltf43s1ftrh!j*43(#d-=v&!lt^6(k!_@0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['opotimeapi.onrender.com']
 
 
 # Application definition
