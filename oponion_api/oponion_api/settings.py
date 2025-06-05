615c6a99ee86a7f4506216345f96cacd76c01e93
TheRealSpeedie <119527242+TheRealSpeedie@users.noreply.github.com>
2025-06-05 08:52:56 +0200

    Update settings.py

--- oponion_api/oponion_api/settings.py
+++ oponion_api/oponion_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-@0mpa!=jdbc23+s!ltf43s1ftrh!j*43(#d-=v&!lt^6(k!_@0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['opotimeapi.onrender.com', '127.0.0.1', "http://localhost:3000", "https://opo-time.vercel.app/"]
+ALLOWED_HOSTS = ['opotimeapi.onrender.com', '127.0.0.1', 'localhost', 'http://localhost:3000', 'https://opo-time.vercel.app']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
@@ -61,7 +61,29 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,7 +91,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'oponion_api.urls'
